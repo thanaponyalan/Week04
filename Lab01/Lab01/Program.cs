8705f4b864420e6cfd65723ed43fecd93ba2c5a4
thanaponyalan <thanaponyalan@gmail.com>
2018-09-07 15:06:11 +0700

    Console WriteLine

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,8 +6,8 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
-            Console.Write("Hello");
+            Console.WriteLine("Hello");
+            Console.WriteLine(" World");
         }
     }
 }

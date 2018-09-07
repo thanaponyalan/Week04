789110c0b046c4a90cbdd0f52bf770abce7a88f6
thanaponyalan <thanaponyalan@gmail.com>
2018-09-07 15:52:20 +0700

    numberic : Convert toInt32 Console ReadLine

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -7,9 +7,9 @@ namespace Lab01
         static void Main(string[] args)
         {
             string str;
-            Console.Write("Enter some characters.");
-            str = Console.Read();
-            Console.WriteLine("You entered: " + str);
+            Console.Write("Please enter value 1 : ");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
         }
     }
 }

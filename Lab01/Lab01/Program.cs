9610755201a954a7cc60adf364dae2893102126a
thanaponyalan <thanaponyalan@gmail.com>
2018-09-07 15:33:39 +0700

    Output Practice

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,12 +6,26 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            const double i = 123.456789d;
-            Console.WriteLine("{0:F1}", i);
-            Console.WriteLine("{0:F2}", i);
-            Console.WriteLine("{0:F3}", i);
-            Console.WriteLine("{0:F4}", i);
-            Console.WriteLine("{0:F5}", i);
+            string name = "Hello";
+            Console.WriteLine(String.Format("{0} there. I said {0}! {0}???", name));
+            Console.WriteLine("{2:d} {0:d} {1:d}", 1, 2, 3);
+            Console.WriteLine("Hello " + "World");
+            Console.WriteLine("Here comes a slash \\");
+            Console.WriteLine("|{0, 10}|", 999);
+            Console.WriteLine("|{0,-10}|", 000);
+            Console.WriteLine("The value: {0}.", 500);
+            Console.WriteLine("The value: {0:C}.", 500);
+            Console.WriteLine("{0,-10:F4}", 12.3456789);
+            Console.WriteLine("{0,-10:C}", 12.3456789);
+            Console.WriteLine("{0,-10:E3}", 12.3456789);
+            Console.WriteLine("{0,-10:x}", 65535);  // (x = lower case)
+            Console.WriteLine("{0,-10:X}", 65535);  // (X = upper case)
+            int i;
+            Console.WriteLine("Value\tSquared\tCubed");
+            for (i = 1; i < 10; i++)
+                Console.WriteLine("{0}\t{1}\t{2}", i, i * i, i * i * i);
+            Console.WriteLine("{0:#.###}.", 1234.56789);
+
         }
     }
 }

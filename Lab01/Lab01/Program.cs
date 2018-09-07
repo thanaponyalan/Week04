ffc40b1ee06cb3cda96e04e8eed41c59d148d0b3
thanaponyalan <thanaponyalan@gmail.com>
2018-09-07 15:03:00 +0700

    Console Write

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lab01
 {
@@ -10,6 +6,8 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello");
+            Console.Write("Hello");
         }
     }
 }

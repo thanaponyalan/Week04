2e437637552f686d66defc8b3f607047e6f06bf3
thanaponyalan <thanaponyalan@gmail.com>
2018-09-07 15:24:53 +0700

    Args Format

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,16 +6,13 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("00000000011111111112");
-            Console.WriteLine("12345678901234567890");
-            Console.WriteLine("{0, 0}", 1);
-            Console.WriteLine("{0, 1}", 1);
-            Console.WriteLine("{0, 2}", 1);
-            Console.WriteLine("{0, 3}", 1);
-            Console.WriteLine("{0, 5}", 1);
-            Console.WriteLine("{0, 10}", 1);
-            Console.WriteLine("{0, 15}", 1);
-            Console.WriteLine("{0, 20}", 1);
+            int n = 123456789;
+            Console.WriteLine("{0, 0:E}", n);
+            Console.WriteLine("{0, 0:F}", n);
+            Console.WriteLine("{0, 0:G}", n);
+            Console.WriteLine("{0, 0:N}", n);
+            Console.WriteLine("{0, 0:P}", n);
+            Console.WriteLine("{0, 0:X}", n);
         }
     }
 }

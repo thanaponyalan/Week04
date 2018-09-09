ff4e63547cc2fc1474bd58c0fdf606a3e456b380
thanaponyalan <thanaponyalan@gmail.com>
2018-09-10 01:32:07 +0700

    Variable Properties.

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,19 +6,16 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            string Name, sName;
-            int ID;
-            double GPA;
-            Console.WriteLine("Enter the following information..");
-            Console.Write("Name : ");
-            Name = Console.ReadLine();
-            Console.Write("Lastname : ");
-            sName = Console.ReadLine();
-            Console.Write("ID : ");
-            ID = int.Parse(Console.ReadLine());
-            Console.Write("GPA : ");
-            GPA = double.Parse(Console.ReadLine());
-            Console.WriteLine("\nThis is result..\nName : "+Name+"\nLastname : "+sName+"\nID : "+ID+"\nGPA : {0:F2}",GPA);
+            Console.WriteLine("Data type : \tbyte\tchar\tbool\tsbyte\tshort\tushort\tuint");
+            Console.WriteLine("Size :\t\t" + sizeof(byte)+'\t'+sizeof(char) + '\t' +sizeof(bool) + '\t' +sizeof(sbyte) + '\t' +sizeof(short) + '\t' +sizeof(ushort) + '\t' +sizeof(uint));
+            Console.WriteLine("Minimum Value :\t" + byte.MinValue + '\t' +(int)char.MinValue + '\t'+'\t' +sbyte.MinValue + '\t' +short.MinValue + '\t' +ushort.MinValue + '\t' +uint.MinValue);
+            Console.WriteLine("Maximum Value :" + byte.MaxValue + '\t' + (int)char.MaxValue + '\t' + '\t' + sbyte.MaxValue + '\t' + short.MaxValue + '\t' + ushort.MaxValue + '\t' + uint.MaxValue);
+
+            Console.WriteLine("\nData type : \tfloat\t\tdouble\t\t\tdecimal\t\t\t\tlong\t\t\t\tulong");
+            Console.WriteLine("Size :\t\t" + sizeof(float) + "\t\t" + sizeof(double) + "\t\t\t" + sizeof(decimal) + "\t\t\t\t" + sizeof(long) + "\t\t\t\t" + sizeof(ulong));
+            Console.WriteLine("Minimum Value :\t" + float.MinValue + "\t" + double.MinValue + '\t' + decimal.MinValue + '\t' + long.MinValue + '\t' + ulong.MinValue);
+            Console.WriteLine("Maximum Value :\t"+ float.MaxValue + '\t' + double.MaxValue + '\t' + decimal.MaxValue + '\t' + long.MaxValue + '\t' + ulong.MaxValue);
+
         }
     }
 }

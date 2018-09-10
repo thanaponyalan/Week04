1850cbda4f803376bebaf7d5dfe8a4cd31cc980f
thanaponyalan <thanaponyalan@gmail.com>
2018-09-10 07:05:24 +0700

    truth table

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,14 +6,56 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            int randNo = rand.Next(0,9);
-            int No;
-            Console.Write("Enter Number : ");
-            No = int.Parse(Console.ReadLine());
-            bool a = randNo > No;
-            Console.WriteLine("Random No is greater than Input No : "+a);
-            Console.WriteLine("Random No is {0}, Input No is {1}.",randNo,No);
+            bool A, B, Y;
+            Console.WriteLine("      Y = A AND B\t\t      Y = A NAND B");
+            Console.WriteLine("-----------------------\t\t-----------------------");
+            Console.WriteLine("   A      B\t|  Y\t\t   A      B\t|  Y");
+            Console.WriteLine("-----------------------\t\t-----------------------");
+            A = false; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y,!Y);
+            A = false; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
+            A = true; B = false; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
+            A = true; B = true; Y = A & B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
+            Console.WriteLine("-----------------------\t\t-----------------------\n");
+
+            Console.WriteLine("      Y = A OR B\t\t      Y = A NOR B");
+            Console.WriteLine("-----------------------\t\t-----------------------");
+            Console.WriteLine("   A      B\t|  Y\t\t   A      B\t|  Y");
+            Console.WriteLine("-----------------------\t\t-----------------------");
+            A = false; B = false; Y = A | B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y,!Y);
+            A = false; B = true; Y = A | B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
+            A = true; B = false; Y = A | B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
+            A = true; B = true; Y = A | B;
+            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
+            Console.WriteLine("-----------------------\n");
+
+            Console.WriteLine("      Y = NOT A\t\t\t      Y = A XOR B");
+            Console.WriteLine("-----------------------\t\t-----------------------");
+            Console.WriteLine("   A\t|  Y\t\t\t   A      B\t| Y");
+            Console.WriteLine("-----------------------\t\t-----------------------");
+            B = A = false; Y = !A;
+            Console.Write(" {0}\t| {1}\t\t\t", A,Y);
+            Y = A ^ B;
+            Console.WriteLine(" {0}\t{1}\t| {2}",A,B,Y);
+            A = false; B = true; Y = !A;
+            Console.Write(" {0}\t| {1}\t\t\t", A, Y);
+            Y = A ^ B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = true; B = false; Y = A ^ B;
+            Console.Write(" {0}\t| {1}\t\t\t", A, Y);
+            Y = A ^ B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            A = B = true; Y = A ^ B;
+            Console.Write(" {0}\t| {1}\t\t\t", A, Y);
+            Y = A ^ B;
+            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
+            Console.WriteLine("-----------------------\t\t-----------------------\n");
         }
     }
 }

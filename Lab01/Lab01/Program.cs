9bfb83e4ccdc7b40ed70523617282b65164517a4
thanaponyalan <thanaponyalan@gmail.com>
2018-09-10 07:18:37 +0700

    Integers

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,56 +6,21 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            bool A, B, Y;
-            Console.WriteLine("      Y = A AND B\t\t      Y = A NAND B");
-            Console.WriteLine("-----------------------\t\t-----------------------");
-            Console.WriteLine("   A      B\t|  Y\t\t   A      B\t|  Y");
-            Console.WriteLine("-----------------------\t\t-----------------------");
-            A = false; B = false; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y,!Y);
-            A = false; B = true; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
-            A = true; B = false; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
-            A = true; B = true; Y = A & B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
-            Console.WriteLine("-----------------------\t\t-----------------------\n");
-
-            Console.WriteLine("      Y = A OR B\t\t      Y = A NOR B");
-            Console.WriteLine("-----------------------\t\t-----------------------");
-            Console.WriteLine("   A      B\t|  Y\t\t   A      B\t|  Y");
-            Console.WriteLine("-----------------------\t\t-----------------------");
-            A = false; B = false; Y = A | B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y,!Y);
-            A = false; B = true; Y = A | B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
-            A = true; B = false; Y = A | B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
-            A = true; B = true; Y = A | B;
-            Console.WriteLine(" {0}\t{1}\t| {2}\t\t {0}\t{1}\t| {3}", A, B, Y, !Y);
-            Console.WriteLine("-----------------------\n");
-
-            Console.WriteLine("      Y = NOT A\t\t\t      Y = A XOR B");
-            Console.WriteLine("-----------------------\t\t-----------------------");
-            Console.WriteLine("   A\t|  Y\t\t\t   A      B\t| Y");
-            Console.WriteLine("-----------------------\t\t-----------------------");
-            B = A = false; Y = !A;
-            Console.Write(" {0}\t| {1}\t\t\t", A,Y);
-            Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}",A,B,Y);
-            A = false; B = true; Y = !A;
-            Console.Write(" {0}\t| {1}\t\t\t", A, Y);
-            Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = true; B = false; Y = A ^ B;
-            Console.Write(" {0}\t| {1}\t\t\t", A, Y);
-            Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            A = B = true; Y = A ^ B;
-            Console.Write(" {0}\t| {1}\t\t\t", A, Y);
-            Y = A ^ B;
-            Console.WriteLine(" {0}\t{1}\t| {2}", A, B, Y);
-            Console.WriteLine("-----------------------\t\t-----------------------\n");
+            int a, b, x, y;
+            a = 10;
+            b = 20;
+            x = 5;
+            y = 2;
+            Console.WriteLine("1. {0} + {1} = {2}",a,b,a+b);
+            Console.WriteLine("2. {0} - {1} = {2}", a, b, a - b);
+            Console.WriteLine("3. {0} * {1} = {2}", x, b, x * b);
+            Console.WriteLine("4. {0} / {1} = {2}", y, a, y / a);
+            Console.WriteLine("5. {0} % {1} = {2}", b, y, b % y);
+            Console.WriteLine("6. {0}", y+10%x);
+            Console.WriteLine("7. {0}", a/3*5);
+            Console.WriteLine("8. {0}", 9/2*a);
+            Console.WriteLine("9. {0} % {1} = {2}", y, 8, y%8);
+            Console.WriteLine("10. {0}",100*x+y%2-a);
         }
     }
 }

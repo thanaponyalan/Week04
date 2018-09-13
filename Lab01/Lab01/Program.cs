716a8d9c60bb27589e3bf7ba8a5885262e6d79d4
thanaponyalan <thanaponyalan@gmail.com>
2018-09-13 23:23:30 +0700

    toSunDistance

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,21 +6,21 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
-            int a, b, x, y;
-            a = 10;
-            b = 20;
-            x = 5;
-            y = 2;
-            Console.WriteLine("1. {0} + {1} = {2}",a,b,a+b);
-            Console.WriteLine("2. {0} - {1} = {2}", a, b, a - b);
-            Console.WriteLine("3. {0} * {1} = {2}", x, b, x * b);
-            Console.WriteLine("4. {0} / {1} = {2}", y, a, y / a);
-            Console.WriteLine("5. {0} % {1} = {2}", b, y, b % y);
-            Console.WriteLine("6. {0}", y+10%x);
-            Console.WriteLine("7. {0}", a/3*5);
-            Console.WriteLine("8. {0}", 9/2*a);
-            Console.WriteLine("9. {0} % {1} = {2}", y, 8, y%8);
-            Console.WriteLine("10. {0}",100*x+y%2-a);
+            const double lightSpeed = 186000d;   // miles per second
+            Console.WriteLine("Light speed = {0} Mile Per second", lightSpeed);
+            const double mileTokm = 1.609344;
+            Console.WriteLine("Light speed = {0} km Per second", lightSpeed * mileTokm);
+            const double ToSunDistance = 5945900000d;  // miles
+            Console.WriteLine("ToSunDistance = {0} km", ToSunDistance);
+            double ToSunTimeOfLight = (ToSunDistance/mileTokm) / lightSpeed;  // miles
+            Console.WriteLine("ToSunTimeOfLight = {0} seconds", ToSunTimeOfLight);
+            Console.WriteLine("ToSunTimeOfLight = {0} minutes", ToSunTimeOfLight / 60d);
+            Console.WriteLine("ToSunTimeOfLight = {0} A.U.",toAU(ToSunDistance));
+        }
+
+        static double toAU(double distance)
+        {
+            return 0.000000006685 * distance;
         }
     }
 }

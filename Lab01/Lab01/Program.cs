074c1d91dd01ae47d6104f49d21bdaf6dfdaa7d9
thanaponyalan <thanaponyalan@gmail.com>
2018-09-17 00:48:21 +0700

    Tan Wave

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -8,13 +8,14 @@ namespace Lab01
         {
             for (float i = 0; i < Math.PI * 5.0F; i += 0.3F)
             {
-                Console.WriteLine("The cos of {0,10:F} = {1,-10:F6}" +
-                spaces(Math.Cos(i)) + "*", i, Math.Cos(i));
+                Console.WriteLine("The tan of {0,10:F} = {1,-10:F6}" +
+                spaces(Math.Tan(i)) + "*", i, Math.Tan(i));
             }
         }
         private static string spaces(double val)
         {
-            string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+            val += 11;
+            string SpaceString = new String(' ', ((int)(val)) + 10);
             return SpaceString;
         }
 
